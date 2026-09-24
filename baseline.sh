23767391965789ea2914c23b9cdd86e7d818e4b1
gary <gary313128@gmail.com>
2026-09-24 14:35:31 +0800

    change log interval

--- baseline.sh
+++ baseline.sh
@@ -10,12 +10,14 @@ swanlab login -k iXDM58vG5zuP1CHeKtVPh
 
 python3 train_pretrain.py \
     --save_dir /vault/baseline \
+    --log_interval 10 \
     --seed 824 \
     --use_wandb \
     --wandb_project "Trustworthy-AI-HW-1"
 
 python3 train_full_SFT.py \
     --save_dir /vault/baseline \
+    --log_interval 10 \
     --seed 824 \
     --use_wandb \
     --wandb_project "Trustworthy-AI-HW-1"
