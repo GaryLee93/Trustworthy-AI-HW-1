89efcb7cc2dd59de82bdbf3aad50fdda18f740f7
gary <gary313128@gmail.com>
2026-09-25 16:29:06 +0800

    fix path

--- data_prepare.sh
+++ data_prepare.sh
@@ -5,11 +5,11 @@ python prepare_wiki_pretrain.py \
     --target_mb 2048 \
     --seed 824 \
     --min_length 100 \
-    --out_path /tmp/Trustworthy-AI-HW-1/minimind/dataset/zhtw_wikipedia_pretrain.jsonl
+    --out_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind/dataset/zhtw_wikipedia_pretrain.jsonl
 
 python prepare_tmmluplus_sft.py \
     --seed 824 \
-    --holdcut_frec 0.2 \
+    --holdout_frec 0.2 \
     --revision 1.1 \
     --holdout_out_path /tmp/Trustworthy-AI-HW-1/minimind/dataset/tmmluplus_holdout_check.jsonl \
-    --out_path /tmp/Trustworthy-AI-HW-1/minimind/dataset/tmmluplus_sft.jsonl
+    --out_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind/dataset/tmmluplus_sft.jsonl
