8c5e2c35776bba20bfa5df15784f722b49debf9a
gary <gary313128@gmail.com>
2026-09-25 17:28:16 +0800

    change the path

--- eval.sh
+++ eval.sh
@@ -4,24 +4,26 @@ set -e
 source /tmp/b11902090/miniconda3/etc/profile.d/conda.sh
 conda activate minigpt
 
+OUTPUT_DIR="$1"
+
 cd /tmp/b11902090/Trustworthy-AI-HW-1/minimind/scripts
 python3 convert_model.py
 python3 eval_tmmluplus.py eval \
     --model_path ../../minimind-3 \
-    --output /tmp/b11902090/Trustworthy-AI-HW-1/experiment/eval.json
+    --output "${OUTPUT_DIR}"/eval.json
 
 python3 eval_tmmluplus.py eval \
     --model_path ../../minimind-3 \
     --chat_template \
-    --output /tmp/b11902090/Trustworthy-AI-HW-1/experiment/eval_template.json
+    --output "${OUTPUT_DIR}"/eval_template.json
 
 python3 eval_tmmluplus.py eval \
     --model_path ../../minimind-3 \
     --num_fewshot 5 \
-    --output /tmp/b11902090/Trustworthy-AI-HW-1/experiment/eval_fewshot.json
+    --output "${OUTPUT_DIR}"/eval_fewshot.json
 
 python3 eval_tmmluplus.py eval \
     --model_path ../../minimind-3 \
     --chat_template \
     --num_fewshot 5 \
-    --output /tmp/b11902090/Trustworthy-AI-HW-1/experiment/eval_fewshot_template.json
+    --output "${OUTPUT_DIR}"/eval_fewshot_template.json
