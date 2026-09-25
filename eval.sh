ed0ea3687245156a79a314449ebc979dec21da92
gary <gary313128@gmail.com>
2026-09-25 18:05:03 +0800

    change the path

--- eval.sh
+++ eval.sh
@@ -11,21 +11,21 @@ python3 convert_model.py
 
 cd /tmp/b11902090/Trustworthy-AI-HW-1
 python3 eval_tmmluplus.py eval \
-    --model_path ../../minimind-3 \
+    --model_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind-3 \
     --output "${OUTPUT_DIR}"/eval.json
 
 python3 eval_tmmluplus.py eval \
-    --model_path ../../minimind-3 \
+    --model_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind-3 \
     --chat_template \
     --output "${OUTPUT_DIR}"/eval_template.json
 
 python3 eval_tmmluplus.py eval \
-    --model_path ../../minimind-3 \
+    --model_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind-3 \
     --num_fewshot 5 \
     --output "${OUTPUT_DIR}"/eval_fewshot.json
 
 python3 eval_tmmluplus.py eval \
-    --model_path ../../minimind-3 \
+    --model_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind-3 \
     --chat_template \
     --num_fewshot 5 \
     --output "${OUTPUT_DIR}"/eval_fewshot_template.json
