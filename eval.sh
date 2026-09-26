d4070d93e4bd79fcd3824048443f66fe37c875ac
gary <gary313128@gmail.com>
2026-09-26 12:11:36 +0800

    fix the command bug

--- eval.sh
+++ eval.sh
@@ -13,24 +13,24 @@ mkdir -p "${OUTPUT_DIR}"/eval
 cd /tmp/b11902090/Trustworthy-AI-HW-1
 python3 eval_tmmluplus.py eval \
     --model_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind-3 \
-    --save_predictions "${OUTPUT_DIR}"/eval/p.json
+    --save_predictions "${OUTPUT_DIR}"/eval/p.json \
     --output "${OUTPUT_DIR}"/eval/eval.json
 
 python3 eval_tmmluplus.py eval \
     --model_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind-3 \
     --chat_template \
-    --save_predictions "${OUTPUT_DIR}"/eval/p_template.json
+    --save_predictions "${OUTPUT_DIR}"/eval/p_template.json \
     --output "${OUTPUT_DIR}"/eval/eval_template.json
 
 python3 eval_tmmluplus.py eval \
     --model_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind-3 \
     --num_fewshot 5 \
-    --save_predictions "${OUTPUT_DIR}"/eval/p_fewshot.json
+    --save_predictions "${OUTPUT_DIR}"/eval/p_fewshot.json \
     --output "${OUTPUT_DIR}"/eval/eval_fewshot.json
 
 python3 eval_tmmluplus.py eval \
     --model_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind-3 \
     --chat_template \
     --num_fewshot 5 \
-    --save_predictions "${OUTPUT_DIR}"/eval/p_template_fewshot.json
+    --save_predictions "${OUTPUT_DIR}"/eval/p_template_fewshot.json \
     --output "${OUTPUT_DIR}"/eval/eval_fewshot_template.json
