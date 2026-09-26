5c30571f345c6631ce54407c0f328bf8f19aa73a
gary <gary313128@gmail.com>
2026-09-26 14:53:04 +0800

    change version tag

--- data_prepare.sh
+++ data_prepare.sh
@@ -11,6 +11,6 @@ python prepare_wiki_pretrain.py \
 python prepare_tmmluplus_sft.py \
     --seed 824 \
     --holdout_frac 0.2 \
-    --revision 1.1 \
+    --revision v1.1 \
     --holdout_out_path /tmp/Trustworthy-AI-HW-1/minimind/dataset/tmmluplus_holdout_check.jsonl \
     --out_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind/dataset/tmmluplus_sft.jsonl
