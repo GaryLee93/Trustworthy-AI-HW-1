b3a9616cb567d76d22a97c9b6e712d8cd1a0dc1e
gary <gary313128@gmail.com>
2026-09-26 12:08:25 +0800

    make evaluation into a dir

--- eval.sh
+++ eval.sh
@@ -9,23 +9,28 @@ OUTPUT_DIR="$1"
 cd /tmp/b11902090/Trustworthy-AI-HW-1/minimind/scripts
 python3 convert_model.py
 
+mkdir -p "${OUTPUT_DIR}"/eval
 cd /tmp/b11902090/Trustworthy-AI-HW-1
 python3 eval_tmmluplus.py eval \
     --model_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind-3 \
-    --output "${OUTPUT_DIR}"/eval.json
+    --save_predictions "${OUTPUT_DIR}"/eval/p.json
+    --output "${OUTPUT_DIR}"/eval/eval.json
 
 python3 eval_tmmluplus.py eval \
     --model_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind-3 \
     --chat_template \
-    --output "${OUTPUT_DIR}"/eval_template.json
+    --save_predictions "${OUTPUT_DIR}"/eval/p_template.json
+    --output "${OUTPUT_DIR}"/eval/eval_template.json
 
 python3 eval_tmmluplus.py eval \
     --model_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind-3 \
     --num_fewshot 5 \
-    --output "${OUTPUT_DIR}"/eval_fewshot.json
+    --save_predictions "${OUTPUT_DIR}"/eval/p_fewshot.json
+    --output "${OUTPUT_DIR}"/eval/eval_fewshot.json
 
 python3 eval_tmmluplus.py eval \
     --model_path /tmp/b11902090/Trustworthy-AI-HW-1/minimind-3 \
     --chat_template \
     --num_fewshot 5 \
-    --output "${OUTPUT_DIR}"/eval_fewshot_template.json
+    --save_predictions "${OUTPUT_DIR}"/eval/p_template_fewshot.json
+    --output "${OUTPUT_DIR}"/eval/eval_fewshot_template.json
